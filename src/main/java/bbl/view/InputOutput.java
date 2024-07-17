2f201c009c7e7ced6123ae7483f683303bb29de7
bbl <bbl@host118.MAIN.MFVSEGEI.RU>
2024-07-17 13:42:35 +0300

    HW #37  cli-view Commit #1

---
+++ src/main/java/bbl/view/InputOutput.java
@@ -0,0 +1,129 @@
+package bbl.view;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public interface InputOutput
+{
+	String readString(String prompt);
+	void writeString(String str);
+	default void writeLine(Object obj)
+	{
+		writeString(obj.toString()+"\n");
+	}
+	default <T>T readObject(String prompt, String errorPrompt,Function <String,T> mapper)
+	{
+		T res=null;
+		boolean running=false;
+		do
+		{
+			String str=readString(prompt);
+			running=false;
+			try
+			{
+				res=mapper.apply(str);
+			}
+			catch (RuntimeException e)
+			{
+				writeLine(errorPrompt+" "+e.getMessage());
+				running=true;
+			}
+		}
+		while(running);
+		return res;
+	}
+	
+	default Integer readInt(String prompt,String errorPrompt)
+	{
+		// Entered string must be a number otherwise errorPrompt  
+		Integer res= readObject(prompt,errorPrompt,str->Integer.parseInt(str));
+		return res;
+	}
+	
+
+	
+	default Double readDouble(String prompt,String errorPrompt)
+	{
+		// Entered string must be a number otherwise errorPrompt  
+		Double res= readObject(prompt,errorPrompt,str->Double.parseDouble(str));	
+		return res;
+	}
+	
+	default Double readNumberRange(String prompt,String errorPrompt,double min, double max)
+	{
+		// Entered string must be a number in range [min, max[ otherwise errorPrompt  
+		Double res= readObject(prompt,errorPrompt,str->
+								{
+									Double d=Double.parseDouble(str);
+									if(d<min || d>=max) 
+									{
+										String erStr=String.format("%f < %f or %f >= %f ", d,min,d,max);
+										throw new RuntimeException(erStr);
+									}
+									return d;
+								}
+					);
+		return res;			
+	}
+	
+	default String readStringPredicate(String prompt,String errorPrompt,Predicate<String> predicate) 
+	{
+		// Entered string must match a given predicate
+		String res=readObject(prompt,errorPrompt,str->
+					{
+						if(!predicate.test(str))
+						{
+							throw new RuntimeException("predicate not test");
+						}
+						return str;
+					}
+		
+							 );
+		return res;
+	}
+	
+	default String readStringOptions(String prompt,String errorPrompt,HashSet<String> options) 
+	{
+		// Entered string must  be one out of given options
+		String res=readObject(prompt,errorPrompt,str->
+		{
+			if(!options.contains(str))
+			{
+				String erStr=String.format("%s is not in options", str);
+				throw new RuntimeException(erStr);
+			}
+			return str;
+		}
+
+				 );
+		return res;
+	}
+	
+	default LocalDate readIsoDate(String prompt,String errorPrompt)
+	{
+		// Entered string must be LocalDate in format (yyyy-mm-dd)
+		LocalDate ld=readObject(prompt,errorPrompt,str->LocalDate.parse(str));
+		return ld;
+	}
+	
+	default LocalDate readIsoDateRange(String prompt,String errorPrompt, LocalDate minDate, LocalDate maxDate)
+	{
+		// Entered string must be LocalDate in format (yyyy-mm-dd) in the]minDate , maxDate[
+		LocalDate ld=readObject(prompt,errorPrompt,str->
+									{
+										LocalDate date=LocalDate.parse(str);
+										if(date.compareTo(minDate)<0 || date.compareTo(maxDate)>=0)
+										{
+											String erStr=String.format("date %s is out of range", date.toString());
+											throw new RuntimeException(erStr);
+										}
+										return date;
+									}
+							   );
+		return ld;
+	}
+}
+
---
+++ src/test/java/bbl/view/InputOutputTest.java
@@ -0,0 +1,204 @@
+package bbl.view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import bbl.view.*;
+
+import org.junit.jupiter.api.Test;
+record User(String userName, String password, LocalDate dateLastLogin, String phoneNumber, int numberOfLogins) {} 
+class InputOutputTest
+{
+    InputOutput io=new SystemInputOutput();
+    @Test 
+    void readObjectTest()
+    {
+    	User user=io.readObject("Enter user in format <username>#<password>#<dateLastLogin>#<phone number>#<number of logins",
+    							"Wrong user format", str->
+    							{
+    								String[] tokens = str.split("#");
+    								return new User(tokens[0],tokens[1],LocalDate.parse(tokens[2]), tokens[3], Integer.parseInt(tokens[4]));
+    							}
+    	);
+    	io.writeLine(user);
+    }
+    @Test
+    void readIntTest()
+    {
+    	Integer a=io.readInt("Input integer", "Wrong integer");
+    	io.writeLine("Good - " + a.toString());
+    }
+    
+    @Test
+    void readNumberRangeTest()
+    {
+    	double min=-5.4;
+    	double max =9.0;
+    	String prompt=String.format("Input double [%f %f[:", min,max);
+    	Double a=io.readNumberRange(prompt, "Double is not into range",min, max);
+    	io.writeLine(" " + a.toString());
+    }
+    
+    @Test
+    void readStringPredicateTest()
+    {
+    	String myStr=io.readStringPredicate("Input string less than 4 symbols", 
+    			                            "string have more or equal than 4 symbols",
+    			                            str->str.length()<4);
+    	io.writeLine("Input string " + myStr);
+    }
+    
+    @Test
+    void readStringOptionsTest()
+    {
+    	String one="One";
+    	String two="Two";
+    	String three="Three";
+    	String prompt=String.format("Input:\n %s\n %s\n %s", one,two,three);
+    	HashSet<String> options= new HashSet<String>();
+    	options.add(one);
+    	options.add(two);
+    	options.add(three);
+    	String myStr=io.readStringOptions(prompt, "String", options);
+    	io.writeLine(myStr);
+    }
+    
+    @Test
+    void readLocalDateTest()
+    {
+    	LocalDate ld=io.readIsoDate("Input date", "string is not date");
+    	io.writeLine(ld.toString());
+    }
+    @Test
+    void readIsoDateRangeTest()
+    {
+    	LocalDate minDate=LocalDate.parse("1962-04-12");
+    	LocalDate maxDate=LocalDate.now();
+    	String prompt = String.format("Input date between %s and %s", minDate.toString(),maxDate.toString());
+    	LocalDate date=io.readIsoDateRange(prompt, "error", minDate, maxDate);
+    	io.writeLine(date.toString());    	
+    }
+    
+	@Test
+	void readUserByFields()
+	{
+		
+		// create User object from separate fields and display out
+		// username at least 6 ASCII letters - first Capital, others Lower case
+		// password at least 8 symbols, at least one capital letter, at least one lower case letter,
+		// at least one digit, at least one symbol from "#$*&%"
+		// phone number  - Israel mobile phone
+		// dateLastLogin not after current date
+		// number of logins any positive number
+		User user=io.readObject("Enter user in format <username>#<password>#<dateLastLogin>#<phone number>#<number of logins",
+				"Wrong user format", str->
+						{
+						String[] tokens = str.split("#");
+						if(!isUserName(tokens[0])) tokens[0]=io.readObject("Wrong login. Input login", " ", s->
+							{
+								if (!isUserName(s))
+								{
+									throw new RuntimeException(" ");
+								}
+								return s;	
+							}
+																		  );
+						if(!isPassword(tokens[1])) tokens[1]=io.readObject("Wrong password. Input pasword", " ", s->
+							{
+								if (!isPassword(s))
+								{
+									throw new RuntimeException(" ");
+								}
+								return s;	
+							} 
+																	);
+						LocalDate date=LocalDate.parse(tokens[2]);
+						if(date.compareTo(LocalDate.now())<=0)
+							date=io.readIsoDateRange("Wrong date Input date", " ", LocalDate.of(1, 1, 1), LocalDate.now());
+						
+						if(!isPhoneNumber(tokens[3])) tokens[3]=io.readObject("Wrong phone. Input phone", " ", s->
+						{
+								if (!isPhoneNumber(s))
+								{
+									throw new RuntimeException(" ");
+								}
+								return s;	
+						} 
+						
+																);	
+						int numberOfLogins=Integer.parseInt(tokens[4]);
+						if( numberOfLogins<=0)
+							{
+								
+								String stmp=io.readStringPredicate("Wrong number of logins. Input numbers of logins", " ",
+																	   s->(Integer.valueOf(s)>0)
+																 	 );
+								numberOfLogins=Integer.valueOf(stmp);
+							}
+						return new User(tokens[0],tokens[1],date, tokens[3], numberOfLogins);
+						}
+							   );
+		io.writeLine(user);
+	}
+
+   private boolean isUserName(String str)
+   {
+	   boolean res=false;
+	   if(str!=null && str.matches("([A-Z]{1}[a-z0-9]{1,4})")) res=true;
+	   return res;
+   }
+   
+   private boolean isPassword(String str)
+   {
+	   boolean res=false;
+	   if(str!=null && 
+		  str.length()<=8 &&        	   
+		  isInString(str,"([A-Z]+)") &&
+		  isInString(str,"([a-z]+)") &&	  
+		  isInString(str,"([0-9]+)") &&
+		  isInString(str,"([\\#\\$\\*\\&\\%]+)")) res=true;
+	   return res;
+   }
+   
+   private boolean isPhoneNumber(String str)
+   {
+	   boolean res=false;
+	   if(str!=null && str.matches("\\+972-?5\\d-?\\d{3}-?\\d{2}-?\\d{2}|05\\d-?\\d{3}-?\\d{2}-?\\d{2}")) res=true;
+	   return res;
+   }
+   
+   
+   private boolean isInString(String str, String regex)
+   {
+	   Pattern pattern = Pattern.compile(regex);
+	   Matcher matcher = pattern.matcher (str);
+	  
+	   return matcher.find();
+   }
+@Test
+   void isUserNameTest()
+   {
+	   assertTrue(isUserName("Bbl64"));
+	   assertFalse(isUserName("bbl64"));
+	   assertFalse(isUserName("4bl64"));
+	   assertFalse(isUserName("Bbl1964"));
+	   assertFalse(isUserName(""));
+   }
+   
+   @Test
+   void isPasswordTest()
+   {
+	   assertTrue(isPassword("abc5*ZZ"));
+	   assertFalse(isPassword("abc5_ZZ"));
+	   assertFalse(isPassword("abc%*ZZ"));
+	   assertFalse(isPassword("ABC5*ZZ"));
+	   assertFalse(isPassword("abc5*#$"));
+	   assertFalse(isPassword("abc5*ZZ$&"));
+	   assertFalse(isPassword(""));
+   }
+   
+ }
